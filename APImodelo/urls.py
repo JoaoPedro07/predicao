74c4e10e41c143d8d169b1146c08fdbd194d13d7
Pedro <jpferreirafsa@gmail.com>
2024-12-06 13:04:06 -0300

    adição da rota drf

--- APImodelo/urls.py
+++ APImodelo/urls.py
@@ -22,6 +22,6 @@ from drf_spectacular.views import (SpectacularAPIView, SpectacularSwaggerView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui",),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui",),
     path('', include('Preditor.urls'))
 ]
